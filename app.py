d2ee5c4f7f268950178b8faa78cc80671a61c33f
Neha1529 <49945712+Neha1529@users.noreply.github.com>
2020-10-03 08:04:17 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from flask import Flask, request,render_template
+from joblib import load
+import pickle
+
+from prediction.transformation import transform
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+    
+
+
+
+@app.route('/predict', methods=['POST', 'GET'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    
+    if request.method == 'POST':
+       
+        data = request.form['text']
+        train_tfidf = transform(data)
+    
+        prediction = model.predict(train_tfidf)
+        output = prediction[0]
+        return render_template('index.html', prediction_text='Intent class is  {}'.format(output))
+        
+  
+
+def loadModel():
+    classifier = load(r'prediction/model.joblib')
+    return classifier
+    
+model = loadModel()
+
+if __name__ == '__main__':
+    
+    
+    app.run(host = '0.0.0.0',port="80",debug =True)
+    
+
+
+# In[ ]:
+
+
+
+
---
+++ prediction/transformation.py
@@ -0,0 +1,27 @@
+from joblib import load
+from preprocessing.preprocessing import preprocessing
+
+   
+def load_count_vectorizer():
+    count_vect = load(r'prediction/count_vect.joblib')
+    return count_vect
+
+
+def load_tfidf_transformer():
+    tfidf_transformer = load(r'prediction/tfidf_transformer.joblib')
+    return tfidf_transformer
+
+
+def transform(data):
+    
+    count_vect = load_count_vectorizer()
+    tfidf_transformer = load_tfidf_transformer()
+    
+    preprocessed_data = preprocessing([data],remove_stopwords=True, lemmatization=True, remove_accented=True)
+    
+    train_cv = count_vect.transform(preprocessed_data)
+    
+    X_train_idf = tfidf_transformer.transform(train_cv)
+    
+    return X_train_idf
+
